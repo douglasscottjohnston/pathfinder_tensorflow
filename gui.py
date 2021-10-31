461367839faa6f0c0c8c9d76e809b8bbd1a6f374
Douglas <douglasscottjohnston@gmail.com>
2021-10-30 22:50:25 -0700

    made classes and documentation

---
+++ gui.py
@@ -0,0 +1,10 @@
+# -*- coding: utf8 -*-
+"""
+version: 0.0.1
+author: Douglas Johnston
+description: The GUI for the pathfinder aplication. It should display frame with tiles that the user can click on to draw bariers or make an end point
+"""
+import tkinter
+
+class GUI():
+    pass
---
+++ pathfinder.py
@@ -0,0 +1,15 @@
+# -*- coding: utf8 -*-
+"""
+version: 0.0.1
+author: Douglas Johnston
+description: A pathfinding application that utilises the tensorflow API to find the quickest path from a starting point to a user chosen endpoint, the user will also be able to add roadblocks that will need to be navigated around
+TODO: 
+"""
+import tensorflow as tf
+
+class Pathfinder():
+    pass
+    #global variables
+
+    #functions
+    
